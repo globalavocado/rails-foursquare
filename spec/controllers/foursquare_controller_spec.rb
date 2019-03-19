25dc9a52349c2acbabf7167c31bc32e27dcc8a06
globalavocado <globalavocado@users.noreply.github.com>
2019-03-19 19:47:19 +0000

    removed hard-coding of parameters

--- spec/controllers/foursquare_controller_spec.rb
+++ spec/controllers/foursquare_controller_spec.rb
@@ -5,16 +5,20 @@ RSpec.describe FoursquareController do
 	describe '#index' do
 
 		let(:client) { double }
+		let(:near) { 'EC1' }
+		let(:query) { 'coffee' }
+		let(:params) { { near: near, query: query } }
 
 		before do
 			allow(Foursquare2::Client).to receive(:new).and_return(client)
 		end
 
-		it 'should return http status code 200', :vcr do
-			expect(client).to receive(:search_venues).with(near: 'EC1', query: 'coffee')
-			response = get :index
+		it 'should return http status code 200' do
+			expect(client).to receive(:search_venues).with(near: near, query: query)
+			response = get :index, params: params
 			expect(response).to have_http_status('200')
 		end
+
 	end
 
 end
