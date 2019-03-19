f46fe58697ecfdbfb1a2f09118e28ac574147eea
globalavocado <globalavocado@users.noreply.github.com>
2019-03-19 19:28:27 +0000

    refactored tests to be independent of API key

--- spec/controllers/foursquare_controller_spec.rb
+++ spec/controllers/foursquare_controller_spec.rb
@@ -4,7 +4,14 @@ RSpec.describe FoursquareController do
 	
 	describe '#index' do
 
+		let(:client) { double }
+
+		before do
+			allow(Foursquare2::Client).to receive(:new).and_return(client)
+		end
+
 		it 'should return http status code 200', :vcr do
+			expect(client).to receive(:search_venues).with(near: 'EC1', query: 'coffee')
 			response = get :index
 			expect(response).to have_http_status('200')
 		end
