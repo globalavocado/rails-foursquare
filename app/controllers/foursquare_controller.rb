483a5b4b0b2272518f07aa9fbfb0f6d270f7c1f1
globalavocado <globalavocado@users.noreply.github.com>
2019-03-19 20:08:54 +0000

    http code 400 returns pretty error message

--- app/controllers/foursquare_controller.rb
+++ app/controllers/foursquare_controller.rb
@@ -1,14 +1,15 @@
 class FoursquareController < ApplicationController
 
 	def index
+
+		if params[:near].blank?
+			render json: { error: 'near parameter is required' }, status: :bad_request 
+		else
+			client = Foursquare2::Client.new(client_id: ENV['foursquare_api_id'], client_secret: ENV['foursquare_api_secret'], api_version: '20170623')
+
+			@cafes = client.search_venues(near: params[:near], query: params[:query])
+		end
 		
-		return head :bad_request if params[:near].blank?
-
-		client = Foursquare2::Client.new(client_id: ENV['foursquare_api_id'], client_secret: ENV['foursquare_api_secret'], api_version: '20170623')
-
-		@cafes = client.search_venues(near: params[:near], query: params[:query])
-
-
 	end
 
 end
